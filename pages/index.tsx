a5b1d99977e5074b48c3782cff55e1024fd39c61
asurance <784672616@qq.com>
2021-03-02 18:42:23 +0800

    添加动画效果

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,20 @@ import { ReactElement, useCallback } from 'react'
 import { CreateFactory } from '../utils/DragonBones'
 import SkeleteonJson from '../assets/skeleton.json'
 import TextureJson from '../assets/texture.json'
+import dragonBones from '../dragonbones/dragonBones'
 
 export default function Home(): ReactElement {
     const onClick = useCallback(() => {
         const factory = CreateFactory(SkeleteonJson, TextureJson, 'texture.png')
         const armature = factory.buildArmature('Dragon')!
         const container = document.getElementById('container') as HTMLDivElement
+        (armature.display as HTMLDivElement).style.transform = 'translate(512px,376px)'
+        armature.animation.play()
+        const clock = new dragonBones.WorldClock()
+        clock.add(armature)
+        setInterval(() => {
+            clock.advanceTime(0.1)
+        }, 100)
         container.appendChild((armature.display))
     }, [])
     return (
