33fff9e95f1493899828762c8ad43127a53403d0
asurance <784672616@qq.com>
2021-03-03 16:15:59 +0800

    删除next相关内容和资源

--- utils/DragonBones.ts
+++
@@ -1,8 +0,0 @@
-import { DomFactory } from '../libs/DomFactory'
-
-export function CreateFactory(skeletonJson: unknown, textureJson: unknown, textureUrl: string) {
-    const factory = new DomFactory()
-    factory.parseDragonBonesData(skeletonJson)
-    factory.parseTextureAtlasData(textureJson, textureUrl)
-    return factory
-}
